1cb5e52bd7570c4bf3d488e5a8647dc2c9079383
Lae_ <laealt2@gmail.com>
2023-12-26 20:12:17 +0900

    Update image and port in serverstatus.js

--- app/assets/js/serverstatus.js
+++ app/assets/js/serverstatus.js
@@ -8,10 +8,10 @@ const net = require('net')
  * @returns {Promise.<Object>} A promise which resolves to an object containing
  * status information.
  */
-exports.getStatus = function(address, port = 25565){
+exports.getStatus = function(address, port = 30023){
 
     if(port == null || port == ''){
-        port = 25565
+        port = 30023
     }
     if(typeof port === 'string'){
         port = parseInt(port)
