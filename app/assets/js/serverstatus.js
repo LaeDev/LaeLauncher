ad0f16941a45bd2c36f445d44ae5e1bd86b6fde8
Lae_ <laealt2@gmail.com>
2023-12-28 06:02:05 +0900

    Update GitHub and support links in custom.toml

--- app/assets/js/serverstatus.js
+++ app/assets/js/serverstatus.js
@@ -2,7 +2,7 @@ const net = require('net')
 
 /**
  * Retrieves the status of a minecraft server.
- * 
+ *
  * @param {string} address The server address.
  * @param {number} port Optional. The port of the server. Defaults to 25565.
  * @returns {Promise.<Object>} A promise which resolves to an object containing
