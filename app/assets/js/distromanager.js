0014457219c0dc19d59efb58f9ea3aacec2c1303
Lae_ <laealt2@gmail.com>
2023-12-30 16:04:24 +0900

    Update REMOTE_DISTRO_URL in distromanager.js

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/LaeDev/AlcoholicLauncher/main/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://alcoholicserver.kro.kr:8080/Nebula/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
